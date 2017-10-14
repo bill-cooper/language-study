216a9f9a922251f77f9a830a8b04a4832b4acc62
bill-cooper <william.l.cooper1@gmail.com>
2017-10-14 07:03:26 -0500

    adding css and js

---
+++ src/translation-tool/wwwroot/js/app.js
@@ -0,0 +1,41 @@
+﻿angular.module('app', ['ea.treeview'])
+    .controller('logCategoryController', function ($scope, $http, eaTreeViewFactory) {
+        $scope.showCounts = false;
+
+        $http.get(window.location.href + "/api/category")
+        .then(function (response) {
+
+                $scope.model = {
+                    categories: response.data
+                };
+                eaTreeViewFactory.setItems($scope.model.categories, $scope.$id);
+            });
+
+        $scope.show = function(item) {
+            $scope.model.selectedCategory = item.display;
+            $scope.model.selectedCategoryId = item.stateName;
+        };
+
+        $scope.getCounts = function (categoryId) {
+            if (categoryId === undefined) {
+                alert("Please select a category first.");
+            } else {
+                $http.get(window.location.href + "/api/category/getcounts",
+                        {
+                            params: { categoryId: categoryId }
+                        })
+                    .then(function(response) {
+                        $scope.model.totalCount = response.data.totalCount;
+                        $scope.model.totalNoKBA = response.data.totalWithNoKBA;
+                        $scope.model.percentNoKBA = response.data.percentWithNoKBA;
+                        $scope.model.totalNoSubscription = response.data.totalWithNoSubscription;
+                        $scope.model.percentNoSubscription = response.data.percentWithNoSubscription;
+                        $scope.showCounts = true;
+                    });
+            }
+        };
+    }).run(function ($rootScope, eaTreeViewFactory) {
+    $rootScope.$on('$stateChangeSuccess', function (event, args) {
+        eaTreeViewFactory.resetItemTemplateUrl();
+    });
+});
